681a4305180433a3ae1a070ec3e6d818a27fbf14
Volodymyr Zakalyk <volodymyr.zakalyk@gmail.com>
2024-01-29 14:08:20 +0200

    Tests, Docs, Product: Add identifiers (#253)

---
+++ bin/test/test_argapp.py
@@ -0,0 +1,13 @@
+from .core import *
+
+def test_0():
+    assert Arg
+    assert App
+    assert ArgHelper
+    assert AppHelper
+    assert Completer
+    assert CompleterNone
+    assert CompleterList
+    assert CompleterPath
+    assert CallError
+    assert main
