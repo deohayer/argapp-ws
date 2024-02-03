8a03cdf300d7fca803d0fc78c62426c824b90989
Volodymyr Zakalyk <volodymyr.zakalyk@gmail.com>
2024-02-03 22:09:39 +0200

    Docs, Tests, Product: Define AppHelper.section_args(...) (#267)

---
+++ bin/test/test_AppHelper_section_args.py
@@ -0,0 +1,36 @@
+from .core import *
+
+
+def test_returns_1_0():
+    assert AppHelper().section_apps('Title', []) == ''
+
+
+def test_returns_2_0():
+    arg = Arg(name='ARGUMENT', help='help')
+    opt = Arg(lopt='opt', help='h\ne\nl\np')
+    act = AppHelper().section_args(
+        'Title',
+        [arg, opt])
+    exp = str(
+        'Title:\n'
+        ' * ARGUMENT  - help\n'
+        ' * --opt OPT - h\n'
+        '               e\n'
+        '               l\n'
+        '               p')
+    assert act == exp
+
+
+def test_returns_2_1():
+    arg = Arg(name='ARGUMENT', help='help')
+    opt = Arg(lopt='opt', help='h\ne\nl\np')
+    act = AppHelper().section_args(
+        '',
+        [arg, opt])
+    exp = str(
+        ' * ARGUMENT  - help\n'
+        ' * --opt OPT - h\n'
+        '               e\n'
+        '               l\n'
+        '               p')
+    assert act == exp
